b83e0f8eb5dfe0006ba30ee82cb4faf3e4442b78
Rodolphe PELLOUX-PRAYER <rodolphe@damsy.net>
2015-11-02 23:39:26 +0100

    Première étape vers Flask

---
+++ bano/frontend/views.py
@@ -0,0 +1,10 @@
+from flask import (Blueprint, render_template, current_app)
+
+frontend = Blueprint('frontend', __name__)
+
+
+@frontend.route('/')
+def index():
+    current_app.logger.debug('debug')
+
+    return render_template('index.html')
