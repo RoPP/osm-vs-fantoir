ea96eaa3684491043bb0bac1132c261925f6eb72
Rodolphe PELLOUX-PRAYER <rodolphe@damsy.net>
2015-11-04 22:36:24 +0100

    Ajout du modèle StatutFantoir

--- bano/models.py
+++ bano/models.py
@@ -4,7 +4,13 @@ from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION
 from .extensions import db
 
 
+class StatutFantoir(db.Model):
+    __tablename__ = 'statut_fantoir'
 
+    fantoir = Column(db.String(10), index=True, primary_key=True)
+    id_statut = Column(db.Integer)
+    timestamp_statut = Column(DOUBLE_PRECISION)
+    insee_com = Column(db.String(5), index=True)
 
 
 class LabelsStatutsFantoir(db.Model):
--- sql/create_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE statut_fantoir (
-    fantoir character varying(10),
-    id_statut integer,
-    timestamp_statut double precision,
-    insee_com character(5));
-
-CREATE INDEX idx_statut_fantoir_insee ON statut_fantoir(insee_com);
-CREATE INDEX idx_statut_fantoir_fantoir ON statut_fantoir(fantoir);
